8311505a92647ed91e1ba17db5f9d09377e15a6f
AlexZav1327 <alexey.zarapin@gmail.com>
2023-07-20 10:42:36 +0300

    refactor: move dialogues to single variable

--- internal/gameplay/game.go
+++ internal/gameplay/game.go
@@ -27,14 +27,6 @@ type DefaultConfiguration struct {
 	MaxNum     int
 }
 
-func NewDefaultConfiguration() *DefaultConfiguration {
-	return &DefaultConfiguration{
-		GuessLimit: 10,
-		MinNum:     1,
-		MaxNum:     1000,
-	}
-}
-
 func NewGame(guess int, set *GameSettings) *Game {
 	return &Game{
 		guessLeft: guess,
@@ -50,16 +42,26 @@ func NewGameSettings(limit int, min int, max int) *GameSettings {
 	}
 }
 
-func (g *Game) HandleProcessMessage(userMessage string) (string, bool) {
-	botAnswerOptions := map[string]string{
-		"guessRange":     fmt.Sprintf("Я загадал число в диапазоне от %d до %d", g.settings.minNum, g.settings.maxNum),
-		"notInt":         "Введено не число. Повтори попытку",
-		"numberTooBig":   fmt.Sprintf("Твое число слишком БОЛЬШОЕ. Число оставшихся попыток: %d", g.guessLeft-1),
-		"numberTooSmall": fmt.Sprintf("Твое число слишком МАЛЕНЬКОЕ. Число оставшихся попыток: %d", g.guessLeft-1),
-		"playerWon":      "О, да! У тебя ПОЛУЧИЛОСЬ отгадать число!\nЧтобы сыграть еще жми /start",
-		"playerLost":     fmt.Sprintf("Извини, у тебя не получилось отгадать число. Ответ был %d.\nЧтобы сыграть еще жми /start", g.target), //nolint:lll
+func NewDefaultConfiguration() *DefaultConfiguration {
+	return &DefaultConfiguration{
+		GuessLimit: 10,
+		MinNum:     1,
+		MaxNum:     1000,
 	}
+}
 
+var botAnswerOptions = map[string]string{ //nolint:gochecknoglobals
+	"guessRange":     "Я загадал число в диапазоне от %d до %d",
+	"notInt":         "Введено не число. Повтори попытку",
+	"numberTooBig":   "Твое число слишком БОЛЬШОЕ. Число оставшихся попыток: %d",
+	"numberTooSmall": "Твое число слишком МАЛЕНЬКОЕ. Число оставшихся попыток: %d",
+	"playerWon":      "О, да! У тебя ПОЛУЧИЛОСЬ отгадать число!\nЧтобы сыграть еще жми /start",
+	"playerLost":     "Извини, у тебя не получилось отгадать число. Ответ был %d.\nЧтобы сыграть еще жми /start",
+	"start":          "Тогда погнали! У тебя %d попыток. Отправь мне число",
+	"rejection":      "Если передумаешь, жми /start",
+}
+
+func (g *Game) HandleProcessMessage(userMessage string) (string, bool) {
 	var botAnswer string
 
 	showResponseKeyboard := false
@@ -67,26 +69,24 @@ func (g *Game) HandleProcessMessage(userMessage string) (string, bool) {
 
 	switch {
 	case userMessage == "/start":
-		botAnswer = botAnswerOptions["guessRange"]
+		botAnswer = fmt.Sprintf(botAnswerOptions["guessRange"], g.settings.minNum, g.settings.maxNum)
 		showResponseKeyboard = true
-
 	case err != nil:
 		botAnswer = botAnswerOptions["notInt"]
 	case userMessageAsInt > g.target:
-		botAnswer = botAnswerOptions["numberTooBig"]
-
 		g.guessLeft--
+
+		botAnswer = fmt.Sprintf(botAnswerOptions["numberTooBig"], g.guessLeft)
 	case userMessageAsInt < g.target:
-		botAnswer = botAnswerOptions["numberTooSmall"]
-
 		g.guessLeft--
+
+		botAnswer = fmt.Sprintf(botAnswerOptions["numberTooSmall"], g.guessLeft)
 	case userMessageAsInt == g.target:
 		botAnswer = botAnswerOptions["playerWon"]
 	}
 
 	if g.guessLeft == 0 {
-		botAnswer = botAnswerOptions["playerLost"]
-
+		botAnswer = fmt.Sprintf(botAnswerOptions["playerLost"], g.target)
 		g.guessLeft = g.settings.guessLimit
 	}
 
@@ -94,11 +94,6 @@ func (g *Game) HandleProcessMessage(userMessage string) (string, bool) {
 }
 
 func (g *Game) HandleProcessCallbackQuery(callbackData string) string {
-	botAnswerOptions := map[string]string{
-		"start":     fmt.Sprintf("Тогда погнали! У тебя %d попыток. Отправь мне число", g.settings.guessLimit),
-		"rejection": "Если передумаешь, жми /start",
-	}
-
 	var botAnswer string
 
 	switch {
@@ -106,7 +101,7 @@ func (g *Game) HandleProcessCallbackQuery(callbackData string) string {
 		g.target = g.generateRandNum()
 		g.guessLeft = g.settings.guessLimit
 
-		botAnswer = botAnswerOptions["start"]
+		botAnswer = fmt.Sprintf(botAnswerOptions["start"], g.settings.guessLimit)
 	case callbackData == "no":
 		botAnswer = botAnswerOptions["rejection"]
 	}
